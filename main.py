be75fa256c1a11dbc8f729750ee2c882055ef4e8
Renat <renat.zaharev@gmail.com>
2024-08-21 14:39:02 +0400

    All example files added

---
+++ main.py
@@ -0,0 +1,40 @@
+# test git
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DIR = os.getenv('DIR')
+
+# https://www.youtube.com/watch?v=VJm_AjiTEEc
+
+# PS I:\my_python> cd I:\my_python\my_programs\обучение\git_learning
+# PS I:\my_python\my_programs\обучение\git_learning> git init
+# Initialized empty Git repository in I:/my_python/my_programs/обучение/git_learning/.git/
+# PS I:\my_python\my_programs\обучение\git_learning> git remote add origin https://github.com/RenatZahar/git_learning.git
+# PS I:\my_python\my_programs\обучение\git_learning> 
+
+
+# копия репо
+# cd -> папка
+# git clone <url>
+
+# проверка статуса репо
+# git status
+
+# подготовка к коммиту файлов
+# git add README.MD
+# git add .
+
+# старт коммита 
+# git commit -m 'Added only README'
+
+# просмотр истории
+# git log 
+
+# просмотр веток
+# git branch
+
+# возврат к версии или к ветке 
+# git checkout <commit №/ имя ветки>
+
